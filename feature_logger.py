1b8f5d31ea402bc5db82618cb8e727bb59cbaab5
gonzalo-cordova-pou <gonzalocp6@gmail.com>
2023-04-12 18:28:23 -0400

    fix: feature extractor logger deployed

--- feature_logger.py
+++ feature_logger.py
@@ -9,25 +9,43 @@ from mlmodeling import *
 
 # Global variables
 INTERVAL = 10 # interval in seconds
-SCREENSHOT_PATH = 'screenshots/' # path to save screenshots
 SCREENSHOT_COUNTER = 1
-LOG_PATH = 'logs/'
-CLASSIFIER_PATH = 'classifier/'
-MODEL = "model_with_extended_dataset_resnet50_2023-03-27_16-08-23.pth"
+DEPLOYMENT_PATH = os.path.join(os.getcwd(), '..', 'deployment')
+CLASSIFIER_PATH = os.path.join(os.getcwd(), '..', 'models')
+MODEL = "model_with_extended_dataset_resnet50_2023-03-27_13-23-33"
 SESSION_ID = None
+LOG_PATH = None
 
-classifier = classifier = MLADHD()
-classifier.load_model(os.path.join(CLASSIFIER_PATH, MODEL))
 
-if not os.path.exists(SCREENSHOT_PATH):
-    os.makedirs(SCREENSHOT_PATH)
+model_name = "model_with_extended_dataset"
+with open(os.path.join(CLASSIFIER_PATH, MODEL+".json"), "r") as fp:
+    hyperparams = json.load(fp)
+classifier = classifier = MLADHD(model_name, None, CLASSIFIER_PATH, hyperparams)
+classifier.load_model(os.path.join(CLASSIFIER_PATH, MODEL+".pth"))
+
+SCREENSHOT_PATH =  None
 
 # Mention the installed location of Tesseract-OCR in your system
-pytesseract.pytesseract.tesseract_cmd = 'C:\\Users\\Gonzalo\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = "E:\\Users\\ADHD Project\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe"
 
 def start_session():
+    
+    global SESSION_ID
+    global SCREENSHOT_PATH
+    global LOG_PATH
+    global SCREENSHOT_COUNTER
+
     if SESSION_ID is None:
         SESSION_ID = time.strftime('%Y%m%d_%H%M%S')
+    
+    SCREENSHOT_PATH =  os.path.join(DEPLOYMENT_PATH, SESSION_ID, "screenshots")
+    if not os.path.exists(SCREENSHOT_PATH):
+        os.makedirs(SCREENSHOT_PATH)
+    
+    LOG_PATH = os.path.join(DEPLOYMENT_PATH, SESSION_ID, "log")
+    if not os.path.exists(LOG_PATH):
+        os.makedirs(LOG_PATH)
+
     # Create a csv log file for the current session
     file = open(os.path.join(LOG_PATH, f"{SESSION_ID}.csv"), "w")
     # Write the header: timestamp , text , prediction , probability
@@ -35,13 +53,13 @@ def start_session():
     file.close()
 
 def take_screenshot():
-    filename = f"screenshot_{SCREENSHOT_COUNTER}_{time.strftime('%Y%m%d_%H%M%S')}.jpg"
-    pyautogui.screenshot(os.path.join(SCREENSHOT_PATH, filename), format='jpg')
+    global SCREENSHOT_COUNTER
+    filename = f"screenshot_{SESSION_ID}_{SCREENSHOT_COUNTER}_{time.strftime('%Y%m%d_%H%M%S')}.jpg"
+    pyautogui.screenshot(os.path.join(SCREENSHOT_PATH, filename))
     SCREENSHOT_COUNTER += 1
     return filename
 
-def process_screenshot(filename):
-    img = cv2.imread(filename)
+def process_screenshot(img):
 
     # Crop 20px from each side of the image, except 60px from the bottom
     img = img[0:img.shape[0] - 60, 20:img.shape[1] - 20]
@@ -115,8 +133,8 @@ def distraction_detection(img):
     pred, prob = classifier.predict(img, raw_output=True)
     return pred, prob
 
-def log(log_dir, session_id, text, pred, prob):
-    file = open(os.path.join(log_dir, f"{session_id}.csv"), "a")
+def log(log_dir, screenshot_filename, text, pred, prob):
+    file = open(os.path.join(log_dir, f"{SESSION_ID}.csv"), "a")
     file.write(f"{time.strftime('%Y%m%d_%H%M%S')}|{text}|{pred}|{prob}")
     file.close()
 
@@ -127,18 +145,19 @@ def main():
         print("Screenshot #", SCREENSHOT_COUNTER)
         screenshot_filename = take_screenshot()
         print("Processing screenshot ({}).".format(screenshot_filename))
-        img = process_screenshot(screenshot_filename)
-        contours = bounding_box(img)
+        img = cv2.imread(os.path.join(SCREENSHOT_PATH, screenshot_filename))
+        dilated_img = process_screenshot(img)
+        contours = bounding_box(dilated_img)
         text = OCR(img, contours, include_annotations=False)
         print("Text extracted")
         print("Detecting distraction")
-        pred, prob = distraction_detection(img)
+        pred, prob = distraction_detection(os.path.join(SCREENSHOT_PATH, screenshot_filename))
         if pred == 1:
             print("Predicted: distracted")
         else:
             print("Predicted: focused")
         
-        log(LOG_PATH, SESSION_ID, screenshot_filename, text, pred, prob)
+        log(LOG_PATH, screenshot_filename, text, pred, prob)
 
         elapsed = time.time() - start
         sleeping = INTERVAL - elapsed
@@ -147,5 +166,4 @@ def main():
             print("Sleeping for ", round(sleeping, 2), "seconds")
             time.sleep(INTERVAL - (time.time() - start))
 
-if __name__ == "__main__":
-    main()
+main()
