0221e76c93d795808624a5011b7c9835089c6ae9
gonzalo-cordova-pou <gcp@vincesconsulting.com>
2023-04-12 12:13:18 -0400

    feat: NEW script for session screenome logging

---
+++ feature_logger.py
@@ -0,0 +1,151 @@
+import cv2
+import numpy as np
+import pytesseract
+import time
+import os
+import pyautogui
+from mlmodeling import *
+
+
+# Global variables
+INTERVAL = 10 # interval in seconds
+SCREENSHOT_PATH = 'screenshots/' # path to save screenshots
+SCREENSHOT_COUNTER = 1
+LOG_PATH = 'logs/'
+CLASSIFIER_PATH = 'classifier/'
+MODEL = "model_with_extended_dataset_resnet50_2023-03-27_16-08-23.pth"
+SESSION_ID = None
+
+classifier = classifier = MLADHD()
+classifier.load_model(os.path.join(CLASSIFIER_PATH, MODEL))
+
+if not os.path.exists(SCREENSHOT_PATH):
+    os.makedirs(SCREENSHOT_PATH)
+
+# Mention the installed location of Tesseract-OCR in your system
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Users\\Gonzalo\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe'
+
+def start_session():
+    if SESSION_ID is None:
+        SESSION_ID = time.strftime('%Y%m%d_%H%M%S')
+    # Create a csv log file for the current session
+    file = open(os.path.join(LOG_PATH, f"{SESSION_ID}.csv"), "w")
+    # Write the header: timestamp , text , prediction , probability
+    file.write("timestamp|screenshot|text|prediction|probability\n")
+    file.close()
+
+def take_screenshot():
+    filename = f"screenshot_{SCREENSHOT_COUNTER}_{time.strftime('%Y%m%d_%H%M%S')}.jpg"
+    pyautogui.screenshot(os.path.join(SCREENSHOT_PATH, filename), format='jpg')
+    SCREENSHOT_COUNTER += 1
+    return filename
+
+def process_screenshot(filename):
+    img = cv2.imread(filename)
+
+    # Crop 20px from each side of the image, except 60px from the bottom
+    img = img[0:img.shape[0] - 60, 20:img.shape[1] - 20]
+
+    # Convert the image to gray scale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    blur = cv2.GaussianBlur(gray, (7,7), 0)
+
+    # Performing OTSU threshold
+    ret, thresh1 = cv2.threshold(blur, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY_INV)
+
+    # Specify structure shape and kernel size.
+    # Kernel size increases or decreases the area
+    # of the rectangle to be detected.
+    # A smaller value like (10, 10) will detect
+    # each word instead of a sentence.
+    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
+    
+    # Applying dilation on the threshold image
+    dilation = cv2.dilate(thresh1, rect_kernel, iterations = 2)
+
+    return dilation
+
+def bounding_box(img):
+    # Finding contours
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL,
+                                                    cv2.CHAIN_APPROX_NONE)
+    # Sort all the contours avoiding annotations
+    # by x-coordinate (left to right), then y-coordinate (top to bottom), then area (big to small)
+    lambda_sort = lambda x: (cv2.boundingRect(x)[0], cv2.boundingRect(x)[1], cv2.contourArea(x))
+    contours = sorted(contours, key=lambda_sort)
+    return contours
+
+def OCR(img, contours, include_annotations=False):
+    
+    im2 = img.copy()
+
+    OCRtext = ""
+
+    # Looping through the identified contours
+    # Then rectangular part is cropped and passed on
+    # to pytesseract for extracting text from it
+    # Extracted text is then written into the text file
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        
+        # If the area of the rectangle is more than 95% of the image area, ignore
+        if (w * h) / (img.shape[0] * img.shape[1]) > 0.95:
+            continue
+
+        # Cropping the text block for giving input to OCR
+        cropped = im2[y:y + h, x:x + w]
+
+        # Apply OCR on the cropped image
+        text = pytesseract.image_to_string(cropped)
+
+        if len(text) > 3:
+            OCRtext += text + "\n"
+
+            if include_annotations:
+                # Creating a rectangle around the identified text
+                rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (36, 255, 12), 2)
+            
+            if include_annotations:
+                return OCRtext, im2
+            
+            return OCRtext
+
+def distraction_detection(img):
+    pred, prob = classifier.predict(img, raw_output=True)
+    return pred, prob
+
+def log(log_dir, session_id, text, pred, prob):
+    file = open(os.path.join(log_dir, f"{session_id}.csv"), "a")
+    file.write(f"{time.strftime('%Y%m%d_%H%M%S')}|{text}|{pred}|{prob}")
+    file.close()
+
+def main():
+    start_session()
+    while True:
+        start = time.time()
+        print("Screenshot #", SCREENSHOT_COUNTER)
+        screenshot_filename = take_screenshot()
+        print("Processing screenshot ({}).".format(screenshot_filename))
+        img = process_screenshot(screenshot_filename)
+        contours = bounding_box(img)
+        text = OCR(img, contours, include_annotations=False)
+        print("Text extracted")
+        print("Detecting distraction")
+        pred, prob = distraction_detection(img)
+        if pred == 1:
+            print("Predicted: distracted")
+        else:
+            print("Predicted: focused")
+        
+        log(LOG_PATH, SESSION_ID, screenshot_filename, text, pred, prob)
+
+        elapsed = time.time() - start
+        sleeping = INTERVAL - elapsed
+        print("Logged | Time elapsed: ", round(elapsed, 2), "seconds")
+        if time.time() - start < INTERVAL:
+            print("Sleeping for ", round(sleeping, 2), "seconds")
+            time.sleep(INTERVAL - (time.time() - start))
+
+if __name__ == "__main__":
+    main()
