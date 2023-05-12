381c8cc21761bb87f8e2139d3451cae4cfe3897c
gonzalo-cordova-pou <gcp@vincesconsulting.com>
2023-05-12 12:27:25 -0400

    feat: Topic mdoeling ready - documentation and last updates

--- NLPutils.py
+++ NLPutils.py
@@ -13,6 +13,7 @@ from nltk.corpus import stopwords
 def load_model():
     return gensim.models.KeyedVectors.load_word2vec_format('./wiki.en.vec')
 
+# Keywords for each defined topic
 topic_keywords = {
     'ComputerScience': ['algorithms', 'data', 'programming', 'networks', 'databases', 'security', 'software', 'development', 'efficiency','backend', 'frontend', 'web', 'robotics', 'computation', 'computational', 'code', 'run', 'debug', 'input',  'output', 'technology', 'overflow', 'binary', 'bit', 'bits', 'byte', 'bytes', 'computer', 'computers',  'computing', 'program', 'programs', 'programming', 'programmer', 'programmers', 'programmed', 'automation', 'devops', 'cloud', 'architecture', 'servers', 'testing', 'debugging', 'scaling', 'data structures', 'analysis', 'analytics', 'design patterns', 'machine learning', 'deep learning', 'neural networks', 'data science',  'database management', 'database design', 'data mining', 'data modeling', 'data warehousing', 'data visualization',  'data analysis', 'data-driven', 'information', 'information systems', 'artificial intelligence', 'computer vision', 'natural language processing', 'cybersecurity', 'cryptography', 'encryption', 'decryption', 'hacking', 'penetration testing', 'firewalls', 'authentication', 'authorization', 'access control', 'virtualization', 'cloud computing', 'cloud storage', 'distributed computing', 'parallel computing', 'high-performance computing', 'scalability', 'optimization', 'operating systems', 'system administration', 'mobile development', 'web development', 'responsive design',  'user interface', 'user experience', 'agile', 'project management', 'version control', 'continuous integration',  'continuous delivery', 'source code', 'open source', 'intellectual property', 'licensing', 'patents', 'copyright', 'data privacy', 'online privacy', 'e-commerce', 'web services', 'internet of things', 'big data', 'blockchain',  'virtual reality', 'augmented reality', 'quantum computing', '3D printing', 'nanotechnology', 'game development', 'graphics programming', 'rendering', 'compilers', 'interoperability', 'API', 'JSON', 'XML', 'HTTP', 'HTTPS',  'TCP/IP', 'FTP', 'SMTP', 'REST', 'SOAP', 'microservices', 'serverless', 'containerization', 'kubernetes', 'docker', 'ansible', 'terraform', 'jenkins', 'puppet', 'chef', 'salt', 'ansible', 'monitoring', 'logging',  'troubleshooting', 'incident response', 'performance tuning', 'load balancing', 'fault tolerance',  'reliability engineering', 'chaos engineering', 'computer graphics', 'computer vision', 'user research',  'virtual assistants', 'machine translation', 'audio processing', 'speech recognition', 'natural language understanding', 'predictive analytics', 'data preprocessing', 'data cleaning', 'data integration', 'data engineering',  'data governance', 'data quality', 'data enrichment', 'data exploration', 'data validation', 'data profiling', 'data storage', 'data migration', 'data synchronization', 'data access', 'data federation', 'data catalog',  'data lineage', 'data lineage', 'data lineage', 'data transformation', 'data curation', 'data lineage',  'data masking', 'data anonymization', 'data ethics'],
     'Chemistry': ['atoms', 'molecules', 'elements', 'reactions', 'thermodynamics', 'kinetics', 'spectroscopy', 'quantum', 'organic', 'inorganic', 'oxygen', 'co2', 'hydrogen', 'electron', 'electronic', 'electrons', 'proton', 'protons', 'napthol', 'state', 'experiment', 'measurements', 'measurement', 'element', 'acid', 'base', 'pH', 'redox', 'stoichiometry', 'enthalpy', 'entropy', 'gibbs', 'solubility', 'solution', 'equilibrium', 'rate', 'rate law', 'catalysis', 'transition state', 'molecular orbitals', 'periodic table', 'valence', 'bonding', 'hybridization', 'isomers', 'chirality', 'alkanes', 'alkenes', 'alkynes', 'aromatics', 'amines', 'alcohols', 'carbonyls', 'carboxylic acids', 'esters', 'amines', 'amides', 'polymers', 'biomolecules', 'proteins', 'nucleic acids', 'lipids', 'carbohydrates', 'chromatography', 'mass spectrometry', 'infrared spectroscopy', 'UV-Vis spectroscopy', 'NMR spectroscopy', 'X-ray crystallography', 'gas laws', 'ideal gas', 'real gas', 'colligative properties', 'phase diagrams'],
@@ -30,7 +31,7 @@ topic_keywords = {
     'Literature': ['poetry', 'prose', 'fiction', 'nonfiction', 'drama', 'criticism', 'literary', 'postcolonial', 'novel', 'literature', 'film', 'writing', 'reading', 'book', 'shakespeare']
 }
 
-# Group the courses by topic and add them to a dictionary
+# Group the courses by the defined topics
 course_groups = {
     'ComputerScience': ['Algorithms', 'VRdevelopment', 'ComputerScience'],
     'Chemistry': ['Chemistry', 'PhysicalChemistry'],
@@ -48,28 +49,35 @@ course_groups = {
     'Literature': ['Literature', 'ReadingLiterature', 'ReadingFilm'],
 }
 
-def get_vector_centroid(crds1):
+def get_vector_centroid(coords):
     '''
-    Find the centroid of a set of vectors.
-
-    @param crds: A matrix containing all of the vectors.
-
+    Find the centroid of a set of embeddings.
+    @param coords: A matrix of coordinates.
     @return: The centroid of the rows of the matrix crds.
     '''
+
     centroid1 = np.zeros(300)
-
-    for i in range(len(crds1)):
-        centroid1 += crds1[i]
-
-    centroid1 /= float(len(crds1))
-
+    for i in range(len(coords)):
+        centroid1 += coords[i]
+    centroid1 /= float(len(coords))
     return centroid1 
 
 def text2emb(words, model):
+    '''
+    From a list of words, return a list of embeddings.
+    '''
+
     return [model[word] for word in words if word in model]
 
 def nearest_cat(refs, word, model):
+    '''
+    From a list of references, return the nearest category to a given word.
     
+    @param refs: A dictionary of references, where the keys are the categories and the values are the embeddings of the references.
+    @param word: The word to be classified.
+    @param model: The word2vec model.
+    @return: The nearest category to the word and the distance to the category.
+    '''
     emb = model[word]
     d_min = 9999
     
@@ -86,7 +94,14 @@ def nearest_cat(refs, word, model):
     return final_cat, d_min
 
 def get_k_nearest_words_from_cat(centroids, model, k=10):
+    '''
+    From a dictionary of centroids, return the k nearest words to each centroid.
 
+    @param centroids: A dictionary of centroids, where the keys are the categories and the values are the embeddings of the centroids.
+    @param model: The word2vec model.
+    @param k: The number of nearest words to be returned.
+    @return: A dictionary of nearest words, where the keys are the categories and the values are a list of tuples (word, distance).
+    '''
     nearest_words = dict()
     for cat in centroids.keys():
         nearest_words[cat] = []
@@ -99,8 +114,13 @@ def get_k_nearest_words_from_cat(centroids, model, k=10):
                 nearest_words[cat].pop()
     return nearest_words
 
-#Function for basic cleaning/preprocessing texts
 def clean(doc):
+    '''
+    Clean a document by removing punctuation marks, accents, numbers, whitespaces and lowercasing.
+
+    @param doc: The document to be cleaned (string)
+    @return: The cleaned document (string)
+    '''
     # Removal of punctuation marks (.,/\][{} etc) and numbers
     doc = "".join([char for char in doc if char not in string.punctuation and not char.isdigit()])
     # Removal of accents
@@ -110,20 +130,36 @@ def clean(doc):
     # Lowercasing
     return doc.lower()
 
-def sent_to_words(sentences):
+def sent_to_words(sentences, only_nouns=False):
+    '''
+    Tokenize a list of sentences into words.
+
+    @param sentences: A list of sentences.
+    @param only_nouns: If True, only nouns are returned.
+    '''
     for sentence in sentences:
         # deacc=True removes punctuations
         nouns = []
         for word,pos in nltk.pos_tag(nltk.word_tokenize(str(sentence))):
-            #if (pos == 'NN' or pos == 'NNP' or pos == 'NNS' or pos == 'NNPS'):
-            nouns.append(word)
+            if only_nouns:
+                if (pos == 'NN' or pos == 'NNP' or pos == 'NNS' or pos == 'NNPS'):
+                    nouns.append(word)
+            else:
+                nouns.append(word)
         yield(nouns)
 
 def remove_stopwords(texts, stop_words):
     return [[word for word in simple_preprocess(str(doc)) 
              if word not in stop_words] for doc in texts]
-# Group courses into topics
+
 def group_courses(course):
+    '''
+    Given a course, return the topic it belongs to.
+
+    @param course: The course to be classified.
+    @return: The topic the course belongs to.
+    '''
+
     for key, value in course_groups.items():
         if course in value:
             return key
@@ -137,7 +173,9 @@ def get_stopwords():
     return stopwords.words('english')
 
 def get_topic_keywords_embed(model):
-    # Keywords Embeddings
+    '''
+    Returns the dictionary of topic keywords embeddings, where the keys are the topics and the values are the embeddings of the keywords.
+    '''
     topic_keywords_embed = dict()
     for k,l in topic_keywords.items():
         topic_keywords_embed[k] = []
@@ -147,12 +185,24 @@ def get_topic_keywords_embed(model):
     return topic_keywords_embed
 
 def get_topic_centroids(topic_keywords_embed):
+    '''
+    Returns the dictionary of topic centroids, where the keys are the topics and the values are the embeddings of the centroids.
+    '''
     topic_centroids = dict()
     for cat in topic_keywords_embed.keys():
         topic_centroids[cat] = get_vector_centroid(topic_keywords_embed[cat])
     return topic_centroids
 
 def sentence_nearest_cat(words, topic_centroids, model):
+    '''
+    From a list of words, return the nearest category.
+
+    @param words: A list of words.
+    @param topic_centroids: A dictionary of topic centroids, where the keys are the topics and the values are the embeddings of the centroids.
+    @param model: The word2vec model.
+    @return: The nearest category to the words and the distance to the category.
+    '''
+
     emb = text2emb(words, model)
     cent = get_vector_centroid(emb)
     d_min = 9999
@@ -166,6 +216,15 @@ def sentence_nearest_cat(words, topic_centroids, model):
     return final_cat, d_min
 
 def sentence_cats_probs(words, topic_centroids, model, topic_to_idx):
+    '''
+    From a list of words, return the probability of each belonging to each topic.
+    
+    @param words: A list of words.
+    @param topic_centroids: A dictionary of topic centroids, where the keys are the topics and the values are the embeddings of the centroids.
+    @param model: The word2vec model.
+    @param topic_to_idx: A dictionary that maps each topic to an index.
+    @return: A list of probabilities, where the index of each probability corresponds to the index of the topic in the topic_to_idx dictionary.
+    '''
     emb = text2emb(words, model)
     cent = get_vector_centroid(emb)
 
