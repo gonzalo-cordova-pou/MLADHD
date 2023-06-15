dd091f2cc28c58e6f217d745acdb95aff5a155a9
gonzalo-cordova-pou <gcp@vincesconsulting.com>
2023-06-15 21:12:26 +0200

    feat: Finished image2text, script converting dataset to text dataset

--- image2text.py
+++ image2text.py
@@ -3,23 +3,22 @@ import pytesseract
 import cv2
 import csv
 import time
-from feature_logger import process_screenshot, bounding_box, OCR
+from distraction_detector import process_screenshot, bounding_box, OCR
 
-dataset = "data_clean_large"
-limit_per_class = 4292
+dataset = "dataset_name"
+limit_per_class = None
 
 # Path to the folder containing the images
-dataPath = os.path.join('E:\\mladhd\\datasets\\', dataset)
+dataPath = ""
 
 # Path to the folder that will contain the text dataset
-textPath = 'E:\\mladhd\\datasets\\text\\'
+textPath = ""
 
 # Path to the tesseract executable
-# pytesseract.pytesseract.tesseract_cmd = 'C:\\Users\\Gonzalo\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe'
-pytesseract.pytesseract.tesseract_cmd = "E:\\Users\\ADHD Project\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = 'tesseract.exe'
 
 # Create the text dataset csv file
-with open(os.path.join(textPath, f"{dataset}_focused2.csv"), "w", newline='', encoding='utf-8') as fp:
+with open(os.path.join(textPath, f"{dataset}.csv"), "w", newline='', encoding='utf-8') as fp:
     wr = csv.writer(fp, delimiter=';')
     wr.writerow(["class", "text", "image"])
 
@@ -45,18 +44,16 @@ def process_image(image_path):
 
 if __name__ == '__main__':
     # For each class in the dataset
-    for class_name in os.listdir(dataPath):
+    for class_name in os.listdir(dataPath)[1:]:
         # For each image in the class
         c = 0
         for image_name in os.listdir(os.path.join(dataPath, class_name)):
             start = time.time()
             c += 1
-            #if c > limit_per_class:
-            #    break
             print(f"Processing {c}: {image_name}")
             image_path = os.path.join(dataPath, class_name, image_name)
             text = process_image(image_path)
-            with open(os.path.join(textPath, f"{dataset}_focused2.csv"), "a", newline='', encoding='utf-8') as fp:
+            with open(os.path.join(textPath, f"{dataset}.csv"), "a", newline='', encoding='utf-8') as fp:
                 wr = csv.writer(fp, delimiter=';')
                 if class_name == "0_focused":
                     wr.writerow([0, text, image_name])
